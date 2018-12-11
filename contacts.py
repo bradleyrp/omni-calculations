0b109de30b1f987423a86d3ad6d9de01117e507c
Ryan Bradley <bradleyrp@gmail.com>
2018-12-10 22:27:53 -0500

    added essential codes

---
+++ contacts.py
@@ -0,0 +1,239 @@
+
+#!/usr/bin/env python
+
+"""
+Contact code derived from hydrogen_bonding and/or salt_bridges.
+Note that the hydrogen bonding and salt bridges code explicitly codes for three-party interactions of 
+a highly specific nature, whereas this code generalizes this to nearby contacts between two elements
+with (1) the option to specify that the subject is e.g. the protein residues (instead of checking all 
+contacts in the system, which would be costly) and (2) the same exact data structure as the two progenitor
+codes so that later these contacts can be easily summed.
+"""
+
+import os,sys,glob,re,time
+import numpy as np
+import scipy
+import scipy.spatial
+import MDAnalysis
+import numpy as np
+
+from ortho import str_types,status
+from omni import get_automacs,basic_compute_loop,uniquify
+from omni import vecangle,vecnorm,HookHandler
+
+# define the columns for a row in the master dataset
+defn_rowspec = [
+	'subject_resname','subject_resid','subject_atom',
+	'target_resname','target_resid','target_atom']
+defn_bonds_to_reduced = np.array([0,1,3,4])
+defn_rowspec_reduced = [defn_rowspec[i] for i in defn_bonds_to_reduced]
+
+def contacts_framewise(fr,target,subject,vec,**kwargs):
+	"""
+	Compute close contacts using global subject and target coordinates. 
+	Called by the contacts function.
+	"""
+	debug = kwargs.get('debug',False)
+	distance_cutoff = kwargs['distance_cutoff']
+	lenscale = kwargs.get('lenscale',10.0)
+	# ensure that the points are inside the box
+	boxstuff = lambda pts,vec : pts-np.floor(pts/vec)*vec
+	# convert back to advanced indexing
+	aind = lambda x : tuple(x.T)
+	# set the foreground and background points
+	pts_back_unstuffed = np.array(target)
+	pts_fore_unstuffed = np.array(subject)
+	pts_back = boxstuff(pts_back_unstuffed,vec)
+	pts_fore = boxstuff(pts_fore_unstuffed,vec)
+	"""
+	historical note: previously the boxsize to cKDTree had to be twice as long 
+	as the regular box vectors for an unknown reason. this bug was eventually
+	fixed and the cKDTree with a "torus" torus norm works great. the original
+	implementation also implemented a foolish method for boxstuff:
+	  lambda pts,vec : pts-(pts>vec)*vec+(pts<np.array([0.,0.,0.]))*vec
+	which did not account for points that strayed by *two* box vectors!
+	however this is now corrected so we dropped the exception
+	  except: return {'subjects':np.array([]),'targets':np.array([])}
+	note that the exception required a ton of valid-frames tabulation downstream
+	of this code, particularly when synchronizing this calculation with others
+	note that future checks of boxstuff results with the following line
+	  np.any(pts_back>vec) or np.any(pts_back<0)
+	showed that some incoming points were still outside the box for some reason
+	possible related to precision. hence ERROR REMAINS for some frames!
+	"""
+	try: tree = scipy.spatial.ckdtree.cKDTree(pts_back,boxsize=vec)
+	except: return {'subjects':np.array([]),'targets':np.array([])}
+	close,nns = tree.query(pts_fore,k=10,
+		distance_upper_bound=distance_cutoff/lenscale)
+	# index pairs within the cutoff distance
+	close_pairs = np.transpose(np.where(np.all((
+		close<distance_cutoff/lenscale,close>0),axis=0)))
+	# list of close donors
+	close_targets = nns[aind(close_pairs)]
+	close_subjects = close_pairs[:,0]
+	return {'subjects':close_subjects,'targets':close_targets}
+
+def count_reduced_contact(resid,resname_set,bonds,obs,bonds_red,mode='full'):
+	"""
+	Tally up the contacts based on a filter over subject resid and lipid residue name.
+	We have two modes: explicit retains full residue/atom specificify while reduced only tells you if a 
+	bond between two residues exists.
+	Note that this is the kernel of the part of the contact code that counts the unique bonds identified 
+	in the beginning of the contact code.
+	"""
+	if mode=='full':
+		# filter the observations by the protein residue (subject_resid) and target resname
+		#   providing a result
+		which = np.where(np.all((bonds[:,
+			defn_rowspec.index('subject_resid')].astype(int)==
+			resid,np.in1d(bonds[:,defn_rowspec.index('target_resname')],resname_set)),axis=0))
+		result = obs.T[which].sum(axis=0)
+	elif mode=='reduced':
+		# the explicit result has rows over specific bonds (in reduced form, minus atom names)
+		#! deprecated: explicit = obs.T[np.where(np.all((bonds_red[:,
+		#!   rowspec_red.index('subject_resid')].astype(int)==resid,np.in1d(
+		#!     bonds_red[:,rowspec_red.index('target_resname')],resname_set)),axis=0))]
+		# we sum over the rows and only test for non-zero, since we do not care which specific bond is 
+		#   formed, hence returning whether or not there was a bond between protein residue and lipid
+		#! deprecated, incorrect: result = (explicit.sum(axis=0)>1)*1
+		which = np.where(np.all((bonds_red[:,
+			defn_rowspec_reduced.index('subject_resid')].astype(int)==
+			resid,np.in1d(bonds_red[:,defn_rowspec_reduced.index('target_resname')],
+				resname_set)),axis=0))
+		idx,counts = uniquify(bonds_red[which].astype(str))
+		result = obs.T[which][idx].sum(axis=0)
+	else: raise Exception('mode is either full or reduced')
+	return result
+
+def count_reduced_contact_reduced(**kwargs): 
+	"""Decorator for the compute loop."""
+	return count_reduced_contact(mode='reduced',**kwargs)
+	
+def contacts(**kwargs):
+	"""
+	Identify, catalog, and count contacts in a simulation.
+	Note that this code was developed to mimic the data structures used by hydrogen bonding and salt bridging
+	codes, and stores the various contacts up to a very high level of specificity (i.e. the specific residue
+	and atom names).
+	"""
+	sn = kwargs['sn']
+	work = kwargs['workspace']
+	calc = kwargs['calc']
+	debug = kwargs.get('debug',False)
+	run_parallel = kwargs.get('run_parallel',True)
+	# distance cutoff stays in angstroms until the compute function
+	distance_cutoff = calc['specs']['cutoff']
+
+	# get coordinates from the hook
+	default_fetch_coordinates = {
+		'import_target':'codes/fetch_coordinates.py',
+		'function':'fetch_coordinates_subject_target'}
+	fetch_coordinates = kwargs.get(
+		'fetch_coordinates',default_fetch_coordinates)
+	# hooks get the same arguments as compute functions
+	coordinates = HookHandler(meta=kwargs,**fetch_coordinates).solve
+	nframes = coordinates['nframes']
+	vecs = coordinates['vecs']
+	subject = coordinates['uni_selections']['subject']
+	targets = coordinates['uni_selections']['target']
+	times = coordinates['times']
+
+	# save topology for later
+	uni = coordinates['uni']
+	_,idx,counts = np.unique(uni.residues.resnames,return_index=True,return_counts=True)
+	resnames = uni.residues.resnames[np.sort(idx)]
+	resnames_master = np.array(resnames)
+	rescounts = counts[np.argsort(idx)]
+
+	# parallel compute
+	coords_target = coordinates['coordinates_by_selection']['target']
+	coords_subject = coordinates['coordinates_by_selection']['subject']
+	compute_function = contacts_framewise
+	out_args = {'distance_cutoff':distance_cutoff}
+	looper = [dict(fr=fr,target=coords_target[fr],subject=coords_subject[fr],
+		vec=vecs[fr],**out_args) for fr in range(nframes)]
+	incoming = basic_compute_loop(compute_function,looper,run_parallel=run_parallel)
+	# data reduction, get valid frames
+	valid_frames = np.where([len(i['subjects'])>0 for i in incoming])[0]
+	if all([len(incoming[i]['subjects'])==0 for i in valid_frames]):
+		raise Exception('no contacts. maybe your cutoff is too short: %s'%distance_cutoff)
+	obs_by_frames = np.array([len(incoming[i]['subjects']) for i in valid_frames]).astype(int)
+	# concatenate the donor/acceptor indices across all frames
+	subject_cat = np.concatenate([incoming[i]['subjects'] for i in valid_frames]).astype(int)
+	target_cat = np.concatenate([incoming[i]['targets'] for i in valid_frames]).astype(int)
+
+	start_time = time.time()
+	# tabulate each bond observation
+	tabulation = np.transpose((subject.resnames[subject_cat],subject.resids[subject_cat],
+		subject.names[subject_cat],targets.resnames[target_cat],targets.resids[target_cat],
+		targets.names[target_cat],))
+	status('stopwatch: %.1fs'%(time.time()-start_time),tag='compute')
+
+	idx,counts = uniquify(tabulation.astype(str))
+	bonds_catalog = tabulation[idx]
+
+	start_time = time.time()
+	# preallocate bond counts per frame
+	counts_per_frame = np.zeros((len(valid_frames),len(idx)))
+	# hash the binds over the indices
+	bonds_to_idx = dict([(tuple(b),bb) for bb,b in enumerate(bonds_catalog)])
+	frame_lims = np.concatenate(([0],np.cumsum(obs_by_frames)))
+	for fr,i in enumerate(frame_lims[:-1]):
+		status('counting observations per frame',i=fr,looplen=len(valid_frames),
+			tag='compute',start=start_time)
+		obs_this = tabulation[frame_lims[fr]:frame_lims[fr+1]]
+		counts_per_frame[fr][np.array([bonds_to_idx[tuple(o)] for o in obs_this])] += 1
+	status('stopwatch: %.1fs'%(time.time()-start_time),tag='compute')
+	status('done heavy lifting',tag='compute')
+	# note the size of the outgoing data. we could shrink this by discarding atom names
+	status('observation array for cutoff %.1f is %.1fMB'%(
+		distance_cutoff,sys.getsizeof(counts_per_frame)/10**6.),tag='note')
+
+	# package the dataset
+	result,attrs = {},{}
+	# everything is indexed by idx
+	result['bonds'] = bonds_catalog
+	result['observations'] = counts_per_frame
+	result['valid_frames'] = valid_frames
+	result['nframes'] = np.array(nframes)
+	result['resnames'] = resnames_master
+	result['subject_residues_resnames'] = subject.residues.resnames
+	result['target_residues_resnames'] = targets.residues.resnames
+	result['subject_residues_resids'] = subject.residues.resids
+	result['target_residues_resids'] = targets.residues.resids
+	result['nmols'] = rescounts
+	result['times'] = np.array(times)
+	# save the rowspec so we know up to unpack the data correctly
+	attrs['defn_rowspec'] = defn_rowspec
+	attrs['defn_rowspec_reduced'] = defn_rowspec
+
+	#! this needs refactored
+	if False:
+
+		# some basic post-processing common to many of the plots
+		bonds,obs = bonds_catalog,counts_per_frame
+		# post: generate timewise trajectories for the number of contacts between protein residues and lipids
+		# methodology note: in a basic version of this calculation we simply count all of the bonds between 
+		#   any lipid-protein residue pair. this means that being more close to a lipid might result in more 
+		#   contacts and hence generates a higher score. hence we have two versions of the calculation. one 
+		#   counts the total number of contacts, and the other discards atom information and scores contacts 
+		#   with a maximum of one per protein residue-lipid pair. this calculation does both
+		#! need to check for atom-name resolution otherwise this is moot
+		resids = result['subject_residues_resids']
+		lipid_resnames = np.unique(bonds[:,defn_rowspec.index('target_resname')])
+		resname_combos = [(r,np.array([r])) for r in lipid_resnames]+[('all lipids',np.array(lipid_resnames))]
+		# reduce the data for the modified count described above
+		bonds_red = bonds[:,defn_bonds_to_reduced]
+		looper = [{'resid':resid,'resname_set':resname_set,'bonds':bonds,'obs':obs,'bonds_red':bonds_red} 
+			for resid in resids for resname_name,resname_set in resname_combos]
+		compute_function = count_reduced_contact
+		incoming = basic_compute_loop(compute_function,looper,run_parallel=run_parallel)
+		# package this as a list of resid/resname pairs and the counts for them
+		result['pairs_resid_resname'] = np.array([(resid,resname_name) 
+			for resid in resids for resname_name,resname_set in resname_combos]).astype(str)
+		result['counts_resid_resname'] = np.array(incoming)
+		compute_function = count_reduced_contact_reduced
+		incoming = basic_compute_loop(compute_function,looper,run_parallel=run_parallel)
+		result['counts_resid_resname_singleton'] = np.array(incoming)
+	
+	return result,attrs
---
+++ lipid_abstractor.py
@@ -0,0 +1,227 @@
+#!/usr/bin/env python
+
+import time,re
+import numpy as np
+import MDAnalysis
+from codes import mesh
+from omni import checktime,basic_compute_loop,status
+
+def lipid_abstractor(grofile,trajfile,**kwargs):
+
+	"""
+	LIPID ABSTRACTOR
+	Reduce a bilayer simulation to a set of points.
+	"""
+
+	arrest = kwargs.pop('arrest',False)
+	sn = kwargs['sn']
+	work = kwargs['workspace']
+	parallel = kwargs.get('parallel',False)
+	# prepare universe	
+	# note that the universe throws a UserWarning on coarse-grained systems
+	#   which is annoying to elevate to error stage and handled below without problems
+	uni = MDAnalysis.Universe(grofile,trajfile)
+	nframes = len(uni.trajectory)
+	# MDAnalysis uses Angstroms not nm
+	lenscale = 10.
+	# select residues of interest
+	selector = kwargs['calc']['specs']['selector']
+	nojumps = kwargs['calc']['specs'].get('nojumps','')
+	#! if the separator depends on the selector you can put it there
+	#! note that the script must accomodate multiply looping formats in the meta
+	separator = selector.get('separator',kwargs['calc']['specs'].get('separator',{}))
+
+	# center of mass over residues
+	if 'type' in selector and selector['type'] == 'com' and 'resnames' in selector:
+		resnames = selector['resnames']
+		selstring = '('+' or '.join(['resname %s'%i for i in resnames])+')'
+	elif 'type' in selector and selector['type'] == 'select' and 'selection' in selector:
+		if 'resnames' not in selector: raise Exception('add resnames to the selector')
+		selstring = selector['selection']
+	elif selector.get('type',None)=='custom':
+		custom_exec_vars = dict(uni=uni,selector=selector)
+		exec(selector['custom'],globals(),custom_exec_vars)
+		selstring = custom_exec_vars['selstring']
+	else: raise Exception('\n[ERROR] unclear selection %s'%str(selector))
+
+	# compute masses by atoms within the selection
+	sel = uni.select_atoms(selstring)
+	if len(sel)==0: raise Exception('empty selection')
+	mass_table = {'H':1.008,'C':12.011,'O':15.999,'N':14.007,'P':30.974,'S':32.065}
+	missing_atoms_aamd = list(set([i[0] for i in sel.atoms.names if i[0] not in mass_table]))
+	if any(missing_atoms_aamd): 
+		print('[WARNING] missing mass for atoms %s so we assume this is coarse-grained'%missing_atoms_aamd)
+		# MARTINI masses
+		mass_table = {'C':72,'N':72,'P':72,'S':45,'G':72,'D':72,'R':72}
+		missing_atoms_cgmd = list(set([i[0] for i in sel.atoms.names if i[0] not in mass_table]))
+		if any(missing_atoms_cgmd):
+			raise Exception('we are trying to assign masses. if this simulation is atomistic then we are '+
+				'missing atoms "%s". if it is MARTINI then we are missing atoms "%s"'%(
+				missing_atoms_aamd,missing_atoms_cgmd))
+		else: masses = np.array([mass_table[i[0]] for i in sel.atoms.names])
+	else: masses = np.array([mass_table[i[0]] for i in sel.atoms.names])
+	
+	# note that the following sequence has been reworked to reflect apparent changes in the 
+	#   residue-handling. previously we used `if len(sel.resids)==len(np.unique(sel.resids)):` but this
+	#   is now incompatible
+	resids = sel.residues.resids
+	# create lookup table of residue indices
+	if len(resids)==len(np.unique(resids)):
+		divider = [np.where(sel.resids==r) for r in np.unique(resids)]
+	# note that redundant residue numbering requires special treatment
+	else:
+		#! note that the resid handling change above may not have been implemented in the custom method below
+		if (('type' in selector) and (selector['type'] in ['com','select']) and ('resnames' in selector)):
+			# note that MDAnalysis sel.residues *cannot* handle redundant numbering
+			#   note also that some test cases have redundant residues *and* adjacent residues with
+			#   the same numbering. previously we tried a method that used the following sequence:
+			#     divider = [np.where(np.in1d(np.where(np.in1d(
+			#       uni.select_atoms('all').resnames,resnames))[0],d))[0] for d in divider_abs]
+			#   however this method is flawed because it uses MDAnalysis sel.residues and in fact
+			#   since it recently worked, RPB suspects that a recent patch to MDAnalysis has broken it
+			#   note that rpb started a method to correct this and found v inconsistent MDAnalysis behavior
+			#   the final fix is heavy-handed: leaving nothing to MDAnalysis subselections
+			allsel = uni.select_atoms('all')
+			lipids = np.where(np.in1d(allsel.resnames,np.array(selector['resnames'])))[0]
+			resid_changes = np.concatenate(([-1],np.where(
+				allsel[lipids].resids[1:]!=allsel[lipids].resids[:-1])[0]))
+			residue_atomcounts = resid_changes[1:]-resid_changes[:-1]
+			# get the residue names for each lipid in our selection by the first atom in that lipid
+			# the resid_changes is prepended with -1 in the unlikely (but it happened) event that 
+			#   a unique lipid leads this list (note that a blase comment dismissed this possibility at 
+			#   first!) and here we correct the resnames list to reflect this. resnames samples the last 
+			#   atom in each residue from allsel 
+			resnames = np.concatenate((allsel[lipids].resnames[resid_changes[1:]],
+				[allsel[lipids].resnames[-1]]))
+			guess_atoms_per_residue = np.array(zip(resnames,residue_atomcounts))
+			# get consensus counts for each lipid name
+			atoms_per_residue = {}
+			for name in np.unique(resnames):
+				# get the most common count
+				counts,obs_counts = np.unique(guess_atoms_per_residue[:,1][
+					np.where(guess_atoms_per_residue[:,0]==name)[0]].astype(int),return_counts=True)
+				atoms_per_residue[name] = counts[obs_counts.argmax()]
+			# faster method
+			resid_to_start = np.transpose(np.unique(allsel.resids,return_index=True))
+			resid_to_start = np.concatenate((resid_to_start,[[resid_to_start[-1][0]+1,len(lipids)]]))
+			divider = np.array([np.arange(i,j) 
+				for i,j in np.transpose((resid_to_start[:,1][:-1],resid_to_start[:,1][1:]))])
+			# make sure no molecules have the wrong number of atoms
+			if not set(np.unique([len(i) for i in divider]))==set(atoms_per_residue.values()):
+				status('checking lipid residue indices the careful way',tag='warning')
+				# the following method is slow on large systems. we use it when the fast method above fails
+				# iterate over the list of lipid atoms and get the indices for each N-atoms for each lipid
+				counter,divider = 0,[]
+				while counter<len(lipids):
+					status('indexing lipids',
+						i=counter,looplen=len(lipids),tag='compute')
+					# until the end, get the next lipid resname
+					this_resname = allsel.resnames[lipids][counter]
+					if selector['type']=='select':		
+						# the only way to subselect here is to select on each divided lipid (since 
+						#   the procedure above has correctly divided the lipids). we perform the 
+						#   subselection by pivoting over indices
+						#! this method needs checked
+						this_inds = np.arange(counter,counter+atoms_per_residue[this_resname])
+						this_lipid = allsel[lipids][this_inds]
+						this_subsel = np.where(np.in1d(this_lipid.indices,this_lipid.select_atoms(
+							selector['selection']).indices))[0]
+						divider.append(this_inds[this_subsel])
+					else: divider.append(np.arange(counter,counter+atoms_per_residue[this_resname]))
+					counter += atoms_per_residue[this_resname]
+				# in the careful method the sel from above is broken but allsel[lipids] is correct
+				sel = allsel[lipids]
+				masses = np.array([mass_table[i[0]] for i in sel.atoms.names])
+		else: raise Exception('residues have redundant resids and selection is not the easy one')
+
+	# load trajectory into memory	
+	trajectory,vecs = [],[]
+	for fr in range(nframes):
+		status('loading frame',tag='load',i=fr,looplen=nframes)
+		uni.trajectory[fr]
+		trajectory.append(sel.positions/lenscale)
+		#! critical fix: you must cast the dimensions or you get repeated vectors
+		vecs.append(np.array(uni.trajectory[fr].dimensions[:3]))
+	vecs = np.array(vecs)/lenscale
+
+	# parallel compute
+	looper = [dict(coords=trajectory[fr],masses=masses,divider=divider) for fr in range(nframes)]
+	coms = basic_compute_loop(mesh.centroid,looper,run_parallel=parallel)
+	if arrest=='coms': return coms
+
+	# identify leaflets
+	status('identify leaflets',tag='compute')
+	leaflet_finder_trials = separator.get('trials',3)
+	# preselect a few frames, always including the zeroth
+	selected_frames = [0]+list(np.random.choice(np.arange(1,nframes),leaflet_finder_trials,replace=False))
+    # alternate lipid representation is useful for separating monolayers
+	if 'lipid_tip' in separator:
+		tip_select = separator['lipid_tip']
+		#! instead of making the tip selection a composition of the selection and the tip, it is independent
+		#!   which is essential if you have a difficult separation e.g. if the lipids are headless. you can
+		#!   put the separator at the top of the specs or inside each selector to customize this properly
+		sel = uni.select_atoms(tip_select)
+		atoms_separator = []
+		for fr in selected_frames:
+			uni.trajectory[fr]
+			atoms_separator.append(sel.positions/lenscale)
+	# default is to use the centers of mass to distinguish leaflets
+	else: atoms_separator = [coms[fr] for fr in selected_frames]
+	# pass frames to the leaflet finder, which has legacy and cluster modes
+	leaflet_finder = mesh.LeafletFinder(
+		atoms_separator=atoms_separator,
+		# pass along the corresponding vectors for topologize
+		vecs=[vecs[i] for i in selected_frames],
+		cluster=separator.get('cluster',False),
+		cluster_neighbors=separator.get('cluster_neighbors',None),
+		topologize_tolerance=separator.get('topologize_tolerance',None))
+	if arrest=='leaflet_finder_fail': 
+		return dict(leaflet_finder=leaflet_finder,selected_frames=selected_frames,vecs=vecs,
+			atoms_separator=atoms_separator,coms=coms)
+	# get the indices from the leaflet finder
+	monolayer_indices = leaflet_finder.monolayer_indices
+
+	# check the right leaflet split. probably easier to check this in a downstream step
+	if 'split' in separator:
+		split_factor = separator['split']
+		leaflet_split = tuple([float(sum(monolayer_indices==i)) for i in [0,1]])
+		leaflet_ratio = float(leaflet_split[1]/(leaflet_split[1]+leaflet_split[0]))
+		if isinstance(separator['split'],tuple) and len(split_factor)==2:
+			if not any([leaflet_split==split_factor[::k] for k in [1,-1]]):
+				raise Exception('leaflet split %s does not match observed %s'%(
+					str(leaflet_split),str(split_factor)))
+		elif isinstance(split_factor,float):
+			if leaflet_ratio!=split_factor and 1./leaflet_ratio!=split_factor:
+				raise Exception('expected leaflet split %.4f and we observed %.4f'%(
+					split_factor,leaflet_ratio))
+		else: raise Exception('invalid split: %s'%str(split_factor))
+	# for convenience when doing planar bilayers we put the zero index on top
+	top_mono = np.argmax([atoms_separator[0][monolayer_indices==i][:,2].mean() for i in range(2)])
+	if top_mono!=0: monolayer_indices = 1-monolayer_indices
+
+	checktime()
+	coms_out = np.array(coms)
+	# remove jumping in some directions if requested
+	if nojumps:
+		nojump_dims = ['xyz'.index(j) for j in nojumps]
+		nobjs = coms_out.shape[1]
+		displacements = np.array([(coms_out[1:]-coms_out[:-1])[...,i] for i in range(3)])
+		for d in nojump_dims:
+			shift_binary = (np.abs(displacements)*(1.-2*(displacements<0))/
+				(np.transpose(np.tile(vecs[:-1],(nobjs,1,1)))/2.))[d].astype(int)
+			shift = (np.cumsum(-1*shift_binary,axis=0)*np.transpose(np.tile(vecs[:-1,d],(nobjs,1))))
+			coms_out[1:,:,d] += shift
+
+	# pack
+	attrs,result = {},{}
+	attrs['selector'] = selector
+	attrs['nojumps'] = nojumps
+	result['resnames'] = np.array(sel.residues.resnames)
+	result['monolayer_indices'] = np.array(monolayer_indices)
+	result['vecs'] = vecs
+	result['nframes'] = np.array(nframes)
+	result['points'] = coms_out
+	result['resids'] = np.array(np.unique(resids))
+	result['resids_exact'] = resids
+	attrs['separator'] = separator
+	return result,attrs	
